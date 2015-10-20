dc3ee99227993fb20efc86edf1b81bc40cd226f0
Katharina Wäschle <waeschle@ub.uni-heidelberg.de>
2015-10-20 14:37:19 +0200

    Added some more demo fields

--- models/tables.py
+++ models/tables.py
@@ -7,7 +7,11 @@ from _dbus_bindings import String
 
 db.define_table("papyri",
     Field("Publikation", type="string", length=200, label="Publikation"),
-    Field('last_modified', 'datetime',
+    Field("Band", type="integer", length=200, label="Band"),
+    Field("Nummer", type="integer", length=200, label="Nummer"),
+    Field("created_on", "datetime",
+          default=request.now, writable=False, label="eingegeben am"),
+    Field("last_modified", "datetime",
           default=request.now, update=request.now, writable=False, label="zuletzt geändert am"),
 #    Field('created_by', 'reference auth_user',
 #          default=auth.user_id, update=auth.user_id, writable=False),
