5f4e303acd121c5f8edf4c5005600c7bfcf9d8dc
Katharina Wäschle <waeschle@ub.uni-heidelberg.de>
2015-11-18 14:13:16 +0100

    Try to catch some irregularities in source files

--- static/tools/pipeline.sh
+++
@@ -1,3 +0,0 @@
-cat GriechAlle.tab.CR | tr "\n" " " | tr "\r" "\n" > GriechAlle.tab.LF.clean
-cat GriechAlle.tab.LF.clean | ./web2py_data.py web2py_data_gr.tab > papyri_new.csv
-mysqlimport --local --verbose --fields-enclosed-by='"' --fields-terminated-by=',' --lines-terminated-by='\n' --user=root --password papyri /home/kw/Dokumente/papyri/papyri_new.csv
--- static/tools/tab2csv.py
+++ static/tools/tab2csv.py
@@ -7,16 +7,16 @@ with open(sys.argv[1]) as f:
 	transformations = [line.strip().split('\t') for line in f.readlines()]
 
 # Get encoding right (hopefully, still need to think about this)
-stdin = sys.stdin #codecs.getreader("utf-8")(sys.stdin)
-stdout = sys.stdout #codecs.getwriter("utf8")(sys.stdout)
+stdin = sys.stdin
+stdout = codecs.getwriter("utf8")(sys.stdout)
 
 # Loop through input rows
 counter = 0
 l = stdin.readline()
 while l:
 	counter += 1
-	entries = l.strip('\n').split('\t')
-	entries_transformed = ['\N'] # no primary key
+	entries = l.decode("utf8").strip('\n').split('\t')
+	entries_transformed = ['"\N"'] # no primary key
 	operations = [a for a, b in transformations]
 	target = operations.count("KEEP")+operations.count("DELETE")
 	if len(entries) != target:
@@ -24,32 +24,36 @@ while l:
 		l = stdin.readline()
 		continue
 
-	# Tarnsform record
+	# Transform record
 	j = 0
 	for t, ttype in transformations:
 		if t == "KEEP":
-			e = entries[j]
+			e = entries[j].replace("\"", "'").replace(u'\x0b', u'\n')
 			if ttype == "boolean":
 				# TODO: refine (depends on possible entries in the fields)
 				if e != "":
-					e = "True"
+					e = "T"
 				else:
-					e = "False"
+					e = "F"
 			if ttype == "list:string":
-				# TODO: split (separator is %0b, see http://www.backbone.se/urlencodingUTF8.htm)
-				pass
+				l = [l.strip() for l in e.split(u'\x1d')]
+				if l:
+					e = "; ".join(l)
 			if ttype == "date" and e != "":
 				try:
-					d, m, y = e.split(".")
+					d, m, y = e.split('.')
+					e = '{:04d}-{:02d}-{:02d}'.format(int(y), int(m), int(d))
 				except:
 					sys.stderr.write("Unabel to convert date: %s\n"%e)
-				e = "{:04d}-{:02d}-{:02d}".format(int(y), int(m), int(d))
-			if e == "":
+			if ttype == "integer" and e != "":
+				# Some BC entries have a blank between "-" and year
+				e = e.replace(" ", "")
+			if e == '':
 				e = '\N'
 			entries_transformed.append('"'+e+'"')
 			j += 1
 		elif t == "INSERT":
-			entries_transformed.append('\N')
+			entries_transformed.append('"\N"')
 		elif t == "DELETE":
 			j += 1
 		elif t == "IGNORE":
@@ -59,4 +63,4 @@ while l:
 
 	# Generate CSV output
 	l = stdin.readline()
-	stdout.write(",".join(entries_transformed)+"\n")
+	stdout.write(','.join(entries_transformed)+'\n')
