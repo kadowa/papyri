e5398b010f50bf622e51d361904e95705ee4a3fc
Katharina Wäschle <waeschle@ub.uni-heidelberg.de>
2016-01-04 14:57:37 +0100

    Remove first output line

--- static/toolbox/getMaxEntryLengthForAllColumns.sh
+++ static/toolbox/getMaxEntryLengthForAllColumns.sh
@@ -10,5 +10,5 @@ read -s -p "Enter MySQL password for $USR: " MYSQL_PW
 # loop over all columns in papyri table, remove first line which only containes "Field"
 for COLUMN_NAME in `echo "DESCRIBE papyri.papyri" | mysql -u $USR --password=$MYSQL_PW | cut -f 1 | sed 1d `; do
 	# get length of longest entry from papyri database, remove first line (contains the count title), add column title on the same line
-	echo "SELECT MAX(LENGTH($COLUMN_NAME)) FROM papyri.papyri;" | mysql -u $USR --password=$MYSQL_PW | sed 1d | paste <(echo "$COLUMN_NAME") -;
-done
+	echo "SELECT MAX(LENGTH($COLUMN_NAME)) FROM papyri.papyri;" | mysql -u $USR --password=$MYSQL_PW | sed 1d | paste <(echo "$COLUMN_NAME") - ;
+done | sed 1d
