29e3cafa033a33785ede7ce627689dce1747816f
Katharina Wäschle <waeschle@ub.uni-heidelberg.de>
2016-06-13 12:40:26 +0200

    Include p_heid_inv_erg, see #9

--- static/toolbox/getIdentifier.sh
+++ static/toolbox/getIdentifier.sh
@@ -7,4 +7,4 @@ read -s -p "Enter MySQL password for $USR: " MYSQL_PW
 # add newline on stderr since we use read -s for security reasons
 >&2 echo ""
 
-echo "SELECT p_heid_inv, p_heid_inv_nr, p_heid_inv_buchstabe, rv FROM papyri.papyri;"  | mysql -u $USR --password=$MYSQL_PW ;
+echo "SELECT p_heid_inv, p_heid_inv_nr, p_heid_inv_buchstabe, p_heid_inv_erg, rv FROM papyri.papyri;"  | mysql -u $USR --password=$MYSQL_PW ;
