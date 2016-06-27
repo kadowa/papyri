bfe0c237e398be28006cc416b8db9f79dea4f0b6
kadowa <waeschle@ub.uni-heidelberg.de>
2016-06-27 12:06:36 +0200

    Standardize rv strings, #11

---
+++ static/toolbox/clean/andere_seite.sql
@@ -0,0 +1,6 @@
+UPDATE `papyri`.`papyri` SET `andere_seite`='Verso frei' WHERE `andere_seite`='Rückseite frei';
+UPDATE `papyri`.`papyri` SET `andere_seite`='Recto frei' WHERE `andere_seite`='Vorderseite frei';
+UPDATE `papyri`.`papyri` SET `andere_seite`='Verso frei vermutlich' WHERE `andere_seite`='Rückseite frei vermutlich';
+UPDATE `papyri`.`papyri` SET `andere_seite`='Recto frei vermutlich' WHERE `andere_seite`='Vorderseite frei vermutlich';
+UPDATE `papyri`.`papyri` SET `andere_seite`='Verso frei vermutlich' WHERE `andere_seite`='Rückseite frei(?)';
+UPDATE `papyri`.`papyri` SET `andere_seite`='Recto frei vermutlich' WHERE `andere_seite`='Vorderseite frei(?)';
---
+++ static/toolbox/clean/rv.sql
@@ -0,0 +1,3 @@
+UPDATE `papyri`.`papyri` SET `rv`='Verso' WHERE `rv`='Rückseite';
+UPDATE `papyri`.`papyri` SET `rv`='Recto' WHERE `rv`='Vorderseite';
+UPDATE `papyri`.`papyri` SET `rv`='Recto u. Verso' WHERE `rv`='Vorderseite u. Rückseite';
