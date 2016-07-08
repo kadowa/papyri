96cc1c1c6bed94ca6764b82337a767ee21b6e186
kadowa <waeschle@ub.uni-heidelberg.de>
2016-07-08 13:34:03 +0200

    Clean p_heid_inv, step 1 #26

---
+++ static/toolbox/clean/clean_p_heid_inv.sql
@@ -0,0 +1,6 @@
+UPDATE papyri.papyri SET p_heid_inv = "Dem." WHERE p_heid_inv = "Dem";
+UPDATE papyri.papyri SET p_heid_inv = "Kopt." WHERE p_heid_inv = "Kopt";
+UPDATE papyri.papyri SET p_heid_inv = "Kopt." WHERE p_heid_inv = "Kopt (?)";
+UPDATE papyri.papyri SET p_heid_inv = "G" WHERE p_heid_inv = "G (?)";
+UPDATE papyri.papyri SET p_heid_inv = "Hebr." WHERE p_heid_inv = "Hebr";
+UPDATE papyri.papyri SET p_heid_inv = "Hier." WHERE p_heid_inv = "hier";
