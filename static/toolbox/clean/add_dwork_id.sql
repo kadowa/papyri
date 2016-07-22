d20be9ee39ea98522cc22fe933c1320d583ec517
kadowa <waeschle@ub.uni-heidelberg.de>
2016-07-22 12:41:10 +0200

    Add column for complete inventory number, close #33

--- static/toolbox/clean/add_dwork_id.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE papyri.papyri ADD project_id VARCHAR(20) DEFAULT NULL AFTER id;
-UPDATE papyri.papyri SET project_id = CONCAT_WS('_', heid_inv_cat, REPLACE(LOWER(heid_inv_spr), '.', ''), heid_inv_nr, heid_inv_buchstabe);
-
