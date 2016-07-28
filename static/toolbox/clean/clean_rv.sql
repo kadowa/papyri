0476d181ee4b8f563f9e726dbddf5521728c7e4c
kadowa <waeschle@ub.uni-heidelberg.de>
2016-07-28 12:12:37 +0200

    Correct records with page combo (fb, f) and (fb, b) but two pages max, #11

--- static/toolbox/clean/clean_rv.sql
+++ static/toolbox/clean/clean_rv.sql
@@ -8,4 +8,10 @@ UPDATE papyri.papyri SET fb = "b" WHERE rv LIKE "Fleischseite%";
 UPDATE papyri.papyri SET fb = "fb" WHERE rv = "Vorderseite u. Rückseite";
 UPDATE papyri.papyri SET fb = "fb" WHERE rv = "Recto u. Verso";
 UPDATE papyri.papyri SET fb = "fb" WHERE rv = "Haarseite u. Fleischseite";
+/* Correct empty values (no idea where they are coming from actually */
 UPDATE papyri.papyri SET `fb` = "f" WHERE `fb` LIKE = "";
+/*  Correct papyri with page combo (fb, f) and (fb, b) but two pages max */
+UPDATE papyri.papyri SET fb="f" WHERE project_id LIKE 'p_kopt_345' AND fb LIKE 'fb';
+UPDATE papyri.papyri SET fb="f" WHERE project_id LIKE 'p_g_57' AND fb LIKE 'fb';
+UPDATE papyri.papyri SET fb="f" WHERE project_id LIKE 'p_g_5048' AND fb LIKE 'fb';
+UPDATE papyri.papyri SET fb="b" WHERE project_id LIKE 'p_g_315' AND fb LIKE 'fb';
