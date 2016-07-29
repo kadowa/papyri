f67b8b2a3c427974f18aae5fed5d08806d52917a
kadowa <waeschle@ub.uni-heidelberg.de>
2016-07-29 11:37:58 +0200

    Set additional fb page record to fb=NULL, #29

--- static/toolbox/clean/clean_rv.sql
+++ static/toolbox/clean/clean_rv.sql
@@ -10,8 +10,10 @@ UPDATE papyri.papyri SET fb = "fb" WHERE rv = "Recto u. Verso";
 UPDATE papyri.papyri SET fb = "fb" WHERE rv = "Haarseite u. Fleischseite";
 /* Correct empty values (no idea where they are coming from actually */
 UPDATE papyri.papyri SET `fb` = "f" WHERE `fb` LIKE "";
-/*  Correct papyri with page combo (fb, f) and (fb, b) but two pages max */
+/* Correct papyri with page combo ('fb', 'f') and ('fb', 'b') but two pages max */
 UPDATE papyri.papyri SET fb="f" WHERE project_id LIKE 'p_kopt_345' AND fb LIKE 'fb';
 UPDATE papyri.papyri SET fb="f" WHERE project_id LIKE 'p_g_57' AND fb LIKE 'fb';
 UPDATE papyri.papyri SET fb="f" WHERE project_id LIKE 'p_g_5048' AND fb LIKE 'fb';
 UPDATE papyri.papyri SET fb="b" WHERE project_id LIKE 'p_g_315' AND fb LIKE 'fb';
+/* Set fb to NULL for papyri that have pages marked 'f', 'b' and 'fb' */
+UPDATE papyri.papyri SET fb=NULL WHERE project_id IN ('p_aram_2', 'p_hebr_10', 'p_hebr_11', 'p_hebr_13', 'p_hebr_14', 'p_hebr_16', 'p_hebr_2', 'p_hebr_21', 'p_hebr_26', 'p_hebr_27', 'p_hebr_28', 'p_hebr_29', 'p_hebr_30', 'p_hebr_31', 'p_hebr_32', 'p_hebr_33', 'p_hebr_4', 'p_hebr_5', 'p_hebr_6', 'p_hebr_7', 'p_hebr_8') AND fb LIKE "fb";
